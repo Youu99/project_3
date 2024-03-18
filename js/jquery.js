be6d1c287abffc42009a7a550a62b4fa364d7d43
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-03-18 15:34:31 +0900

    2024-03-18

--- js/jquery.js
+++ js/jquery.js
@@ -11,3 +11,41 @@ $(function(){
         $(".book_menu_box").eq(idx).fadeIn().css("display","flex").siblings(".book_menu_box").fadeOut()
     })
 })
+
+
+$(function(){
+    $("#top_tab ul li").click(function(){
+        let idx=$(this).index()
+
+        $("#top_tab ul li").removeClass("choice");
+        $(this).addClass("choice");
+        $(".audio_wrap").eq(idx).fadeIn().siblings(".audio_wrap").fadeOut()
+    })
+})
+
+
+// QA 리스트 
+
+$(function(){
+    $("#QA_list_box ul li").click(function(){
+        
+        $(this).find(".QA_icon").toggleClass("active")
+        $(this).find(".QA_list_bottom").slideToggle()
+       
+    })
+})
+
+
+// 푸터 사업자 정보
+
+$(function(){
+    $("#footer_btn").click(function(){
+        $("#footer_btn_UP").show()
+        $("#footer_btn").hide()
+    })
+
+    $("#footer_btnUp").click(function(){
+        $("#footer_btn_UP").hide()
+        $("#footer_btn").show()
+    })
+})
---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
